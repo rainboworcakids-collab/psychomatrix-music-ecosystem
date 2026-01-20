fde6672747a72e7ab786b2ddaa6d947c350dbbd2
rainboworcakids-collab <rainboworcakids@gmail.com>
2026-01-20 10:54:09 +0700

    Add files via upload

--- apps/music-promo-pwa/service-worker.js
+++ apps/music-promo-pwa/service-worker.js
@@ -16,7 +16,12 @@ const PRECACHE_ASSETS = [
   './assets/logo.png',
   './icons/favicon.ico',  
   './icons/icon-72x72.png',
+  './icons/icon-96x96.png',  
+  './icons/icon-128x128.png',
+  './icons/icon-144x144.png',
+  './icons/icon-152x152.png',
   './icons/icon-192x192.png',
+  './icons/icon-384x384.png',
   './icons/icon-152x152.png'
 ];
 
